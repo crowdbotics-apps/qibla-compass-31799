2ed7c48ba957e17274313821a8a673d85639b884
Crowdbotics <team@crowdbotics.com>
2021-11-02 07:53:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "qibla_compass_31799",
+  url: "https://qibla_compass_31799.botics.co",
+  api: "https://qibla_compass_31799.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
